0d9191dff426b74f073d7dbebf72921702aaf019
jaroslawroszyk <roszyk.jarek@gmail.com>
2022-05-29 19:34:34 +0200

    Remove unnecessary function

--- BankManagment/LoginWindow.cpp
+++ BankManagment/LoginWindow.cpp
@@ -16,18 +16,6 @@ LoginWindow::~LoginWindow()
     delete ui;
 }
 
-void LoginWindow::centerAndResize()
-{
-    QSize availableSize = qApp->desktop()->availableGeometry().size();
-       int width = availableSize.width();
-       int height = availableSize.height();
-       qDebug() << "Available dimensions " << width << "x" << height;
-       width *= 0.9; // 90% of the screen size
-       height *= 0.9; // 90% of the screen size
-       qDebug() << "Computed dimensions " << width << "x" << height;
-       QSize newSize( width, height );
-}
-
 void LoginWindow::on_LoginButton_clicked()
 {
     QString username = ui->LoginBox->text();
